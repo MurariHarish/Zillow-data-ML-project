1c853473d8246c348fd907a4b30fffecef2fb677
MurariHarish <harishmeg18@hotmail.com>
2023-11-09 18:24:30 -0500

    dvc for data ingestion

--- src/ZillowHouseData/utils/common.py
+++ src/ZillowHouseData/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.ZillowHouseData.logger import logger
+from ZillowHouseData.logger import logger
 import json
 import joblib
 from ensure import ensure_annotations
